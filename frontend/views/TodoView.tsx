5a885afab8b2b015520653fa6db5cb3b12e8e2ba
imskanand <the.anand3099@gmail.com>
2023-01-30 18:36:49 +0530

    Fetching Todo from the server

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -1,4 +1,12 @@
+import {useEffect, useState} from "react";
+import Todo from "Frontend/generated/com/example/application/Todo";
+import {TodoEndpoint} from "Frontend/generated/endpoints";
+
 export default function TodoView() {
+    const [todos, setTodos] = useState<Todo[]>([])
+    useEffect(()=>{
+        TodoEndpoint.findAll().then(setTodos)
+    },[])
     return (
         <div>
             <h1>
