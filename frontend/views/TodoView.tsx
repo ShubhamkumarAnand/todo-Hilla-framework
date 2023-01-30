3c624cb655ddfdf014ab38c802580f207345a75e
imskanand <the.anand3099@gmail.com>
2023-01-30 19:47:07 +0530

    Adding new todo : input and server calls

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -1,17 +1,35 @@
 import {useEffect, useState} from "react";
 import Todo from "Frontend/generated/com/example/application/Todo";
 import {TodoEndpoint} from "Frontend/generated/endpoints";
+import {TextField} from "@hilla/react-components/TextField.js";
+import {Button} from "@hilla/react-components/Button.js";
 
 export default function TodoView() {
     const [todos, setTodos] = useState<Todo[]>([])
-    useEffect(()=>{
+    const [task, setTask] = useState('')
+
+    useEffect(() => {
         TodoEndpoint.findAll().then(setTodos)
-    },[])
+    }, [])
+
+    async function addTodo() {
+        const saved = await TodoEndpoint.add(task);
+        if (saved) {
+            setTodos([...todos, saved])
+            setTask('')
+        }
+    }
+
     return (
-        <div>
+        <div className="p-m">
             <h1>
                 Here is Hilla View!
             </h1>
+            <div className="flex gap-xl">
+                <TextField value={task} onChange={e => setTask(e.target.value)}/>
+                <Button theme="primary" onClick={addTodo}>Add Task</Button>
+            </div>
+
         </div>
     );
 };
